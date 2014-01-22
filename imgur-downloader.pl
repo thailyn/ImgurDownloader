cc08a07b2fd1e2f35ec663d68941ab93206c4ccd
Charles Macanka <cmacanka@gmail.com>
2014-01-21 20:43:14 -0500

    Initial commit.  Extracts out title from file.
    An input file (html file) is provided as an argument.  Mojo::DOM is used to
    parse the file and find elements.

---
+++ imgur-downloader.pl
@@ -0,0 +1,25 @@
+use strict;
+use Mojo::DOM;
+
+my $inputFile = shift;
+die "Error: Must provide an input file name" unless $inputFile;
+die "Error: Input file must exist" unless -e $inputFile;
+
+open(my $fh, $inputFile) or die "Can not open file $inputFile: $!";
+my $html = do { local $/; <$fh> } ;
+close($fh);
+
+print "All good.  File: $inputFile.\n";
+#print "Contents:\n'$html'";
+
+my $dom = Mojo::DOM->new($html);
+
+my $title_element = $dom->at('title'); #match('html head title');
+if ($title_element) {
+  print "Title: $title_element->text\n";
+}
+else {
+  print "Failed to find title.\n";
+}
+
+print "Done.";
